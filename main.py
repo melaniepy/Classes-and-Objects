2bdb88f4283e4223910e4fb9ac16be00e916ad8a
Mel <101869476+melaniepy@users.noreply.github.com>
2022-05-28 16:26:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+class Student:
 
+    def __init__(self, name, age, tracks, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+
+    def change_name(self, new_name):
+        self.name = new_name
+        return self.name
+    
+    def change_age(self, new_age):
+        self.age = new_age
+        return self.age
+    
+    def add_track(self, new_track):
+        self.track = new_track
+        return self.track
+
+    def get_score(self):
+        return self.score
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+print(Bob.change_name("Peter"))
+print(Bob.change_age(34))
+print(Bob.add_track("UI/UX"))
+print(Bob.get_score())
